82f80031652d803c786edb273acdc52494cd7e37
ATIII <ahtashamahsan988@gmail.com>
2022-07-17 22:32:02 +0500

    wrote javascript for writing a note and updating it in the local storage

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log("included successfully");
+// 
+let addBtn = document.getElementById('addBtn');
+addBtn.addEventListener(
+    'click', function(e){
+        let addTxt = document.getElementById('addTxt');
+        let notes = localStorage.getItem('notes');
+        if(notes == null){
+            notesObj = []
+        }
+        else{
+            notesObj = JSON.parse(notes)
+        }
+        notesObj.push(addTxt.value);
+        localStorage.setItem('notes', JSON.stringify(notesObj));
+        addTxt.value = "";
+        console.log(notes);
+})
