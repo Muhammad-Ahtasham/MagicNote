f3ac71c3e9c3ec803e9d4eede008235ea917cac5
ATIII <ahtashamahsan988@gmail.com>
2022-07-17 22:52:25 +0500

    populating the cards with the notes text

--- js/app.js
+++ js/app.js
@@ -1,17 +1,46 @@
 // 
+showNotes();
 let addBtn = document.getElementById('addBtn');
 addBtn.addEventListener(
-    'click', function(e){
+    'click', function (e) {
         let addTxt = document.getElementById('addTxt');
         let notes = localStorage.getItem('notes');
-        if(notes == null){
+        if (notes == null) {
             notesObj = []
         }
-        else{
+        else {
             notesObj = JSON.parse(notes)
         }
         notesObj.push(addTxt.value);
         localStorage.setItem('notes', JSON.stringify(notesObj));
         addTxt.value = "";
-        console.log(notes);
-})
+        showNotes();
+    })
+
+function showNotes() {
+    let notes = localStorage.getItem('notes');
+    if (notes == null) {
+        notesObj = []
+    }
+    else {
+        notesObj = JSON.parse(notes)
+    }
+    let html = "";
+    notesObj.forEach(function (element, index) {
+        html += `            
+        <div class="card m-2 noteCard" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Note ${index + 1}</h5>
+          <p class="card-text">${element}</p>
+          <button href="#" class="btn btn-primary">Delete Notes</button>
+        </div>
+      </div>`;
+    });
+    let noteElm = document.getElementById('notes')
+    if(notesObj.length!=0){
+        noteElm.innerHTML = html;
+    }
+    else{
+        noteElm.innerHTML = `Nothing to show right now`;
+    }
+}
